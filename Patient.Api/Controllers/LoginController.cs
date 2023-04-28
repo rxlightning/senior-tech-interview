7fd141c6e4a1c23501b8177f1a2b3d3370ebc574
KingxMurphy <charlie.hust4@gmail.com>
2023-04-28 15:26:19 -0400

    initial commit.

---
+++ Patient.Api/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Patient.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // POST api/<LoginController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+    }
+}
---
+++ Patient.Api/Services/PatientService.cs
@@ -0,0 +1,18 @@
+﻿using Patient.Api.Interfaces;
+using Patient.Api.Models;
+
+namespace Patient.Api.Services
+{
+    public class PatientService : IPatientService
+    {
+        public Task<List<PatientDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PatientDto> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
